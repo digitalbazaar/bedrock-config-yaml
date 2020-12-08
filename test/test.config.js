6bb579718743fac96036ec94cabf96eb29284924
Matthew Collier <mcollier@digitalbazaar.com>
2020-12-08 17:10:21 -0500

    Update tests.

--- test/test.config.js
+++ test/test.config.js
@@ -7,11 +7,3 @@ const {config} = require('bedrock');
 const path = require('path');
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
-
-config['config-yaml'].path = path.join(__dirname);
-
-// config for mock test-bedrock-module
-config['test-bedrock-module'] = {
-  bar: 'fromBedrockConfig',
-  overwriteMe: 'fromBedrockConfig',
-};
--- test/test.js
+++ test/test.js
@@ -2,7 +2,18 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 const bedrock = require('bedrock');
+const path = require('path');
+const {config} = bedrock;
 require('bedrock-config-yaml');
+
+config['config-yaml'].app.path = path.join(__dirname, 'mock-configs');
+config['config-yaml'].core.path = path.join(__dirname, 'mock-configs');
+
+// config for mock test-bedrock-module
+config['test-bedrock-module'] = {
+  bar: 'fromBedrockConfig',
+  overwriteMe: 'fromBedrockConfig',
+};
 
 require('bedrock-test');
 bedrock.start();
