68076236a27d762e1cc4dcc64c44ea25d4c8b6e9
Matthew Collier <mcollier@digitalbazaar.com>
2020-12-08 17:10:21 -0500

    Update tests.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -7,7 +7,7 @@ const bedrock = require('bedrock');
 const {config} = bedrock;
 
 describe('bedrock-config-yaml', () => {
-  it('yaml configuration should be merged into bedrock config', () => {
+  it('app yaml configuration should be merged into bedrock config', () => {
     const testBedrockModuleConfig = {
       bar: 'fromBedrockConfig',
       foo: 'fromYaml',
@@ -15,5 +15,10 @@ describe('bedrock-config-yaml', () => {
     };
 
     config['test-bedrock-module'].should.eql(testBedrockModuleConfig);
+  });
+  it('core yaml configuration should be merged into bedrock config', () => {
+    should.exist(config.loggers.console.someLoggerConfig);
+    config.loggers.console.someLoggerConfig.should.be.a('string');
+    config.loggers.console.someLoggerConfig.should.equal('foo');
   });
 });
